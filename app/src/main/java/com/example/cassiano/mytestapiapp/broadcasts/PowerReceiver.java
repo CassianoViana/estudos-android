77038ec2895a892d5a590328b9de5730a4b0dcef
Cassiano Viana <cassiano.pvianna@gmail.com>
2016-12-25 17:06:46 -0200

    First commit

--- app/src/main/java/com/example/cassiano/mytestapiapp/broadcasts/PowerReceiver.java
+++ app/src/main/java/com/example/cassiano/mytestapiapp/broadcasts/PowerReceiver.java
@@ -5,39 +5,41 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.example.cassiano.mytestapiapp.alarm.AlarmReceiver;
 
 public class PowerReceiver extends BroadcastReceiver {
 
-    private AlarmManager alarmManager;
-    private PendingIntent pendingIntentAlarm;
+
 
     @Override
     public void onReceive(Context context, Intent intent) {
+
         String msg = null;
 
-        alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        Intent alarmIntent = new Intent(context, AlarmReceiver.class);
+        PendingIntent pendingIntentAlarm = PendingIntent.getBroadcast(context, 0, alarmIntent, 0);
 
         if (intent.getAction().equals(Intent.ACTION_POWER_CONNECTED)) {
             msg = "Dispositivo conectado";
-            armaAlarme(context);
+            armaAlarme(context, pendingIntentAlarm, alarmManager);
 
         } else if (intent.getAction().equals(Intent.ACTION_POWER_DISCONNECTED)) {
             msg = "Dispositivo desconectado";
-            cancelaAlarme();
+            cancelaAlarme(alarmManager,pendingIntentAlarm);
         }
         Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
     }
 
-    private void armaAlarme(Context context) {
-        Intent alarmIntent = new Intent(context, AlarmReceiver.class);
-        pendingIntentAlarm = PendingIntent.getBroadcast(context, 0, alarmIntent, 0);
-        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 10000, pendingIntentAlarm);
+    private void armaAlarme(Context context, PendingIntent intent, AlarmManager alarmManager) {
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 10000, intent);
     }
 
-    private void cancelaAlarme(){
-        alarmManager.cancel(pendingIntentAlarm);
+    private void cancelaAlarme(AlarmManager alarmManager, PendingIntent intent){
+        Log.d("TAG", "Cancelou");
+        alarmManager.cancel(intent);
     }
 }
